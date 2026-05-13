9f5ba1398c11de2504c608afc36d4a7ec3275611
Abdelrahman <abdelrahman.ayman.tech@gmail.com>
2026-05-13 11:00:14 +0300

    chore: update autogenerated build and project metadata files

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eddea03326bfb1f03a942602d4a7ca0239846890")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0dc49325443ab915cd28f134fd44aa24c78bf48")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
